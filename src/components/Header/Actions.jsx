95f0646dbe01ce091dc607f31839e58de2aca798
Aleksandra Mętrak <aleksandra.metrak@gmail.com>
2024-04-09 10:28:58 +0200

    basic buttons

--- src/components/Header/Actions.jsx
+++ src/components/Header/Actions.jsx
@@ -1,16 +1,25 @@
 // import React from "react";
 import styled from "styled-components";
 import Button from "./Button";
-import IconWandSparkles from "../../assets/icons/IconWandSparkles";
+import IconWandSparkles from "../../assets/icons/WandSparklesIcon";
+import IconCopy from "../../assets/icons/CopyIcon";
 
 const Actions = () => (
-    <StyledSection>
-        <Button label="Generate quote" icon={IconWandSparkles} />
-    </StyledSection>
+  <StyledSection>
+    <Button label="Number of quotes" type="special"></Button>
+    <Button label="Generate quote" type="submit">
+      <IconWandSparkles width=".8em" height=".8em" />
+    </Button>
+    <Button label="Copy all quotes" type="button">
+      <IconCopy width=".9em" />
+    </Button>
+  </StyledSection>
 );
 
 const StyledSection = styled.section`
-    display: flex;
-`
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+`;
 
 export default Actions;
--- src/components/Header/Button.jsx
+++ src/components/Header/Button.jsx
@@ -1,22 +1,31 @@
 // import React from "react";
 import styled from "styled-components";
 
-const Button = ({ label, icon }) => (
+const Button = ({ label, children }) => {
+  return (
     <StyledButton type="button">
-        <span>{label}</span>
-        {icon && icon}
+      <Label>{label}</Label>
+      {children}
     </StyledButton>
-);
+  );
+};
 
 const StyledButton = styled.button`
-    padding: 8px 24px;
-    font-size: 1.15rem;
-    font-variation-settings: "wght" 480;
-    letter-spacing: .2px;
-    background-color: var(--white);
-    border: var(--borderWidth) solid var(--black);
-    border-radius: var(--borderRadius);
-    cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 8px 24px;
+  font-size: 1.15rem;
+  font-variation-settings: "wght" 480;
+  letter-spacing: 0.2px;
+  background-color: var(--white);
+  border: var(--borderWidth) solid var(--black);
+  border-radius: var(--borderRadius);
+  cursor: pointer;
+`;
+
+const Label = styled.span`
+  white-space: nowrap;
 `;
 
 export default Button;
