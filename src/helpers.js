f91c4c137a67ff115feec233e47ac94f29647de2
Aleksandra Mętrak <aleksandra.metrak@gmail.com>
2024-04-23 09:45:05 +0200

    revome permissions from interacting with clipboard

--- src/helpers.js
+++ src/helpers.js
@@ -1,20 +1,30 @@
-import texts from "./texts";
-
 export function copyToClipboard(text) {
-  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
-    if (result.state == "granted" || result.state == "prompt") {
-      navigator.clipboard.writeText(text).then(
-        () => {
-          console.log("Content copied to clipboard");
-          /* Resolved - text copied to clipboard successfully */
-        },
-        () => {
-          console.error("Failed to copy");
-          /* Rejected - text failed to copy to the clipboard */
-        }
-      );
-    } else {
-      alert(texts.errors.clipboard);
+  navigator.clipboard.writeText(text).then(
+    () => {
+      console.log("Content copied to clipboard");
+      /* Resolved - text copied to clipboard successfully */
+    },
+    () => {
+      console.error("Failed to copy");
+      /* Rejected - text failed to copy to the clipboard */
     }
-  });
+  );
+
+  // As of now it works only in chromium based browsers 
+  //   navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+  //     if (result.state == "granted" || result.state == "prompt") {
+  //       navigator.clipboard.writeText(text).then(
+  //         () => {
+  //           console.log("Content copied to clipboard");
+  //           /* Resolved - text copied to clipboard successfully */
+  //         },
+  //         () => {
+  //           console.error("Failed to copy");
+  //           /* Rejected - text failed to copy to the clipboard */
+  //         }
+  //       );
+  //     } else {
+  //       alert(texts.errors.clipboard);
+  //     }
+  //   });
 }
--- src/texts.js
+++ src/texts.js
@@ -5,8 +5,5 @@ export default {
     copy: "Copy quote",
     generate: "Generate quote",
     number: "Number of quotes",
-  },
-  errors: {
-    clipboard: "You have to allow clipboard permission in your browser!"
   }
 };
